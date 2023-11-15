7a1cf27b624e858faa4407ebeb5c67acc9060bdd
dlutskov <dima.lutskov@gmail.com>
2023-11-16 00:26:54 +0200

    Initial commit

---
+++ src/main/java/com/baeldung/websocket/MessageEncoder.java
@@ -0,0 +1,28 @@
+package com.baeldung.websocket;
+
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+import com.baeldung.model.Message;
+import com.google.gson.Gson;
+
+public class MessageEncoder implements Encoder.Text<Message> {
+
+    private static final Gson gson = new Gson();
+
+    @Override
+    public String encode(Message message) throws EncodeException {
+        return gson.toJson(message);
+    }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+        // Custom initialization logic
+    }
+
+    @Override
+    public void destroy() {
+        // Close resources
+    }
+}
